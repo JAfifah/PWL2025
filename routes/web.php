a6735396dc5f05c5518a77cf051a97357833b28f
JAfifah <jamiatul.afifah2735@gmail.com>
2025-02-17 15:34:56 +0700

    js2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PageController;
 
 // Route untuk beranda
@@ -15,3 +14,16 @@ Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 // Route untuk post dan comment
 Route::get('/posts/{post}/comments/{comment}', [PageController::class, 'postComments']);
+
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
